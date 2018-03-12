d6b7bdf7d1f5377fca0c0bcab87d6c2013955a75
Byron <jamesblocker81@gmail.com>
2018-03-12 10:48:40 -0700

    create views

--- ejs-practice-app/app.js
+++ ejs-practice-app/app.js
@@ -1,19 +1,21 @@
 const
     express = require('express'),
     app = express()
+    ejsLayouts = require('express-ejs-layouts')
 
 app.set('views', `${__dirname}/views`)
-
 app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
 
 app.get('/', (req, res) => {
-    var users = [
-        {name: "Jack", email: "jack@gmail.com"},
-        {name: "Jill", email: "jill@gmail.com"},
-        {name: "Pat", email: "pat@gmail.com"}
-    ]
+    // var users = [
+    //     {name: "Jack", email: "jack@gmail.com"},
+    //     {name: "Jill", email: "jill@gmail.com"},
+    //     {name: "Pat", email: "pat@gmail.com"}
+    // ]
 
-    res.render('index', {title: 'Hola, bienvenido', users: users})
+    res.render('index', {title: 'Hola, bienvenido'})
 })
 
 app.listen(3000, (err) => {
